17686c603a4daea5326115c317e8e65c8a2a6c2e
Amit Chauhan <68509577+anshu-ui@users.noreply.github.com>
2024-09-16 14:31:23 +0000

    datatypes

---
+++ Basics/datatype.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newwe version
+
+//alert("hello") // we are using node js not browser (give error)
+
+
+let name = "amit"
+let age  = 54
+let isLoggedIn = true
+
+
+// number  => 2 to power 53
+// bigint 
+// string => ""
+// boolean  => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+// object 
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
