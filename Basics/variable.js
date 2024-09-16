c83c497bb6a1d6eda6accba95f9aef8446847bd0
Amit Chauhan <68509577+anshu-ui@users.noreply.github.com>
2024-09-16 14:15:10 +0000

    container

---
+++ Basics/variable.js
@@ -0,0 +1,23 @@
+// variable is a container to store the values
+const accountId = 14453
+let accountEmail ='amit.1drona'
+var accountPassword = "1245"
+accountCity = "Jaipur"
+
+// const means we cant change the values nor machine 
+
+// accountId = 1 not alllowed
+
+accountEmail = "aghsu@gmail.com"
+accountPassword = "575"
+accountCity = "Banglore"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]) // we can use console.table to run multiple variable in one tabular form
+
+//. var we cant use the var because of scope {} javascript not run without scope
+
+/*
+preferd not to use var
+because of issue on block scope and functional scope
+*/
